7f90cc0c9d8fb8bbc6350a0a9d4d9be3675def34
Guilherme Almeida <noghartt@outlook.com>
2020-03-21 13:02:21 -0300

    fix(api): set API KEY inside baseURL deleting the env

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { API_KEY } from 'react-native-dotenv';
 
 export default (query, page) =>
   axios({
-    baseURL: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`,
+    baseURL: `https://api.themoviedb.org/3/search/movie?api_key=6e09219c6174d28a7f04d7d8467efd6f&language=en-US&query=${query}&page=${page}&include_adult=false`,
     method: 'GET',
   });
