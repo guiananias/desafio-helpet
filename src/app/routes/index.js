aef2bc08d4894bcc1ba482d8c9f3cf795753c698
Guilherme Almeida <noghartt@outlook.com>
2020-03-21 16:45:05 -0300

    feat: add stack screen to movies page

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SearchPage from '../pages/Search';
+import FavoritesPage from '../pages/Favorites';
+import TabNavigationPages from './_TabNavigation';
 
 const RootStack = createStackNavigator();
 
@@ -11,6 +13,8 @@ export default function Routes() {
     <NavigationContainer>
       <RootStack.Navigator screenOptions={{ headerShown: false }}>
         <RootStack.Screen name="Search" component={SearchPage} />
+        <RootStack.Screen name="Main" component={TabNavigationPages} />
+        <RootStack.Screen name="Favorites" component={FavoritesPage} />
       </RootStack.Navigator>
     </NavigationContainer>
   );
