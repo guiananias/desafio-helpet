83718b81d2422fd8597acfc2382f56abbb78bf4e
Guilherme Almeida <noghartt@outlook.com>
2020-03-22 16:49:58 -0300

    chore: change api key inside url to env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
+
+const { API_KEY } = Constants.manifest.extra;
 
 export default (query, page) =>
   axios({
-    baseURL: `https://api.themoviedb.org/3/search/movie?api_key=6e09219c6174d28a7f04d7d8467efd6f&language=en-US&query=${query}&page=${page}&include_adult=false`,
+    baseURL: `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`,
     method: 'GET',
   });
